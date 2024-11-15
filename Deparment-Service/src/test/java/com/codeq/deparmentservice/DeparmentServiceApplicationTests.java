df7724a8e9013bd4730efc757ad0e6c5f2dc2c2a
chayan <chayanchowdhury@gmail.com>
2024-11-15 13:01:14 +0530

    department and employee service implemented

---
+++ Deparment-Service/src/test/java/com/codeq/deparmentservice/DeparmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeq.deparmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeparmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
